f5b35229012c6b634914e89caeef9949abb226a0
MGeraud <geraud.mon@gmail.com>
2019-10-18 18:36:24 +0200

    HumanComparison comparer() method completed

--- src/com/ocr/geraud/Main.java
+++ src/com/ocr/geraud/Main.java
@@ -1,13 +1,14 @@
 package com.ocr.geraud;
 
+import com.ocr.geraud.compareMethods.HumanComparison;
 import com.ocr.geraud.player.AIPlayer;
 import com.ocr.geraud.player.HumanPlayer;
 
 public class Main {
 
     public static void main(String[] args) {
-        HumanPlayer j = new HumanPlayer();
-        j.createSecretPIN();
-        System.out.println(j.getSecretPIN());
+        HumanComparison j = new HumanComparison();
+        j.comparer();
+        System.out.println(j);
     }
 }
--- src/com/ocr/geraud/compareMethods/HumanComparison.java
+++ src/com/ocr/geraud/compareMethods/HumanComparison.java
@@ -3,14 +3,26 @@ package com.ocr.geraud.compareMethods;
 import java.util.Scanner;
 
 public class HumanComparison implements Comparison{
+
     @Override
     public String comparer() {
+        
         String humanComparison;
+        int nullIsNotValid;
+
         do {
+            nullIsNotValid = 0;
+
             System.out.println("Merci d'indiquer pour chacun des chiffres proposés si celui de la combinaison est plus grand (tapez : +), plus petit (tapez -) ou identique (tqpez =) :");
             Scanner sc = new Scanner(System.in);
             humanComparison = sc.nextLine();
-        } while ( humanComparison.length() != 4 );
+            int i=0;
+            do {
+                if (humanComparison.charAt(i) == '=' || humanComparison.charAt(i) == '+' || humanComparison.charAt(i) == '-')
+                    nullIsNotValid++;
+                i = i + 1;
+                }while (i <=3);
+            } while ( humanComparison.length()!=4 || nullIsNotValid != 4);
         return humanComparison ;
     }
 }
