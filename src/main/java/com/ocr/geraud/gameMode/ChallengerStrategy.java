d714b071651a3263fd2a4989aa46e9fb45063834
MGeraud <geraud.mon@gmail.com>
2019-10-31 11:04:32 +0100

    Add log4j2 via Maven Add log4j2.xml config file

--- src/main/java/com/ocr/geraud/gameMode/ChallengerStrategy.java
+++ src/main/java/com/ocr/geraud/gameMode/ChallengerStrategy.java
@@ -8,12 +8,16 @@ import com.ocr.geraud.compareMethods.Comparison;
 import com.ocr.geraud.player.AIPlayer;
 import com.ocr.geraud.player.HumanPlayer;
 import com.ocr.geraud.player.Player;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ChallengerStrategy implements ModeJeuStrategy {
     private Player challenger = new HumanPlayer();
     private Player defender = new AIPlayer();
     private String maxtries = LoadProperties.getInstance().getProperty("MaxTries");
     private int maxTries = Integer.parseInt(maxtries);
+
+    private static final Logger logger = LogManager.getLogger(ChallengerStrategy.class);
 
 
     @Override
@@ -36,6 +40,7 @@ public class ChallengerStrategy implements ModeJeuStrategy {
         } while (!winner && i <= maxTries);
         if (winner) {
             System.out.println("Bravo! Vous avez trouvé la bonne combinaison.");
+            logger.info("Le joueur a trouvé la bonne combinaison.");
         }
         if (i > maxTries) {
             System.out.println("Perdu, vous avez épuisé tous vos essais");
